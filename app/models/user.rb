7ba631d977652240cd7d4655f7e21f32dba0065f
yss44 <nya060@gmail.com>
2013-03-20 11:30:19 +0900

    clear charenging excesize

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 
   has_secure_password
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -75,6 +75,15 @@ describe User do
     it {should_not be_valid}
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email){"Foo@example.COM"}
+    it "should be saved as lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before{@user.password = @user.password_confirmation = ""}
     it{should_not be_valid}
