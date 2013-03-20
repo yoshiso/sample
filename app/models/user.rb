29425d94bd9f246d750d3467e9f88e46b03339c4
yss44 <nya060@gmail.com>
2013-03-20 22:01:41 +0900

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
 
   before_save { email.downcase! }
+  before_save :create_remember_token
 
   has_secure_password
 
@@ -22,4 +23,9 @@ class User < ActiveRecord::Base
                     uniqueness:{case_sensitive:false}
   validates :password,length:{minimum:6}
   validates :password_confirmation,presence:true
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
